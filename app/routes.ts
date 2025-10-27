55e0f810c10afe1d326ded92cb366ffe0838f662
mangle <manglestealth@163.com>
2025-10-27 22:45:51 +0800

    route

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("app/main", "routes/app.main.tsx"),
+  route("fulfillment_order_notification", "routes/fulfillment_order_notification.tsx")
 ] satisfies RouteConfig;
